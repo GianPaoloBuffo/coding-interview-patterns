3545b42e2fbc9f2181b37781483c0e2ffbbdd378
Gian Paolo Buffo <gianpaolo.buffo@uberall.com>
2023-01-15 16:24:13 +0100

    Improve ValidPalindrome

--- src/TwoPointers/ValidPalindrome.java
+++ src/TwoPointers/ValidPalindrome.java
@@ -23,17 +23,16 @@ public class ValidPalindrome {
 
     // Time Complexity: O(n)
     // Space Complexity: O(1)
-    private static boolean isPalindrome(String s) {
+    public static boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
-        boolean isPalindrome = s.charAt(left) == s.charAt(right);
 
-        while (isPalindrome && left < right) {
-            left = left + 1;
-            right = right - 1;
-            isPalindrome = s.charAt(left) == s.charAt(right);
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
         }
 
-        return isPalindrome;
+        return true;
     }
 }
