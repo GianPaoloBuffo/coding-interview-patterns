8b22a0c316aa63cb30f43ded6a5ea5b6c269fa5e
Gian Paolo Buffo <gianpaolo.buffo@uberall.com>
2023-01-16 21:05:31 +0100

    Add ValidPalindromeLenient

---
+++ src/TwoPointers/ValidPalindromeLenient.java
@@ -0,0 +1,39 @@
+package TwoPointers;
+
+public class ValidPalindromeLenient {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("RACEACAR"));
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left++;
+                right--;
+            } else if (isPalindromeStrict(s.substring(left + 1, right + 1))) {
+                return true;
+            } else {
+                return isPalindromeStrict(s.substring(left, right));
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isPalindromeStrict(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
