ab7b5b3b8fd8d8ccb8c4666be9b0ee26322944e5
Gian Paolo Buffo <gpbuffo@gmail.com>
2023-01-28 17:22:09 +0100

    Add MaximumInSlidingWindow

--- src/SlidingWindow/MaximumInSlidingWindow.java
+++ src/SlidingWindow/MaximumInSlidingWindow.java
@@ -10,15 +10,35 @@ public class MaximumInSlidingWindow {
 
         windowSize = Math.min(windowSize, nums.size());
 
-        // todo: implement solution
+        for (int i = 0; i < windowSize; i++) {
+            while (!window.isEmpty() && nums.get(i) >= nums.get(window.peekLast())) {
+                window.removeLast();
+            }
+            window.addLast(i);
+        }
+
+        result.add(nums.get(window.peek()));
+
+        for (int i = windowSize; i < nums.size(); i++) {
+            while (!window.isEmpty() && nums.get(i) >= nums.get(window.peekLast())) {
+                window.removeLast();
+            }
+
+            if (!window.isEmpty() && window.peek() <= i - windowSize) {
+                window.removeFirst();
+            }
+
+            window.addLast(i);
+            result.add(nums.get(window.peek()));
+        }
 
         return result;
     }
 
     public static void main(String[] args) {
-        List<Integer> nums = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> nums = List.of(-3, -2, -5, -6, -1, -4, -8);
         int windowSize = 3;
 
-        findMaxSlidingWindow(nums, windowSize).forEach(System.out::print);
+        findMaxSlidingWindow(nums, windowSize).forEach(v -> System.out.print(v + " "));
     }
 }
